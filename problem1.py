b82a7839fd9a622b3a16f6f54e6c711b0c932764
Anderson Tenório <anderson.sergio@gmail.com>
2019-12-29 17:35:41 -0300

    problem 1 solved

--- problem1.py
+++ problem1.py
@@ -1,9 +1,7 @@
 sum = 0
 for i in range(3, 1000):
-    if i%3 == 0:        
+    if i%3 == 0 or i%5 == 0:        
         sum = sum + i
         continue
-    if i%5 == 0:        
-        sum = sum + i
-        continue
+    
 print(sum)
