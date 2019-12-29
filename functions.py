50fd1b9529c5d4584c360e1337e1407b9c2cf23f
Anderson Tenório <anderson.sergio@gmail.com>
2019-12-29 20:22:39 -0300

    auxiliary functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    is_prime = True
+    if n == 1 or n == 2:
+        is_prime = True
+    elif n > 2 and n % 2 != 0:        
+        for i in range(2, n // 2):
+            if n % i == 0:
+                is_prime = False
+                break
+    else: 
+        is_prime = False
+    return is_prime
+    
+        
---
+++ problem3.py
@@ -0,0 +1,5 @@
+from functions import is_prime
+
+for i in range(1, 100):
+    print(i)
+    print(is_prime(i))
